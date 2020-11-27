365331b4775f49ebcff72e50fba1e187798fe541
Youness <elkhatabi.youness@gmail.com>
2020-11-27 13:48:19 +0100

    add validation form for register

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,6 +8,58 @@
             // Check for post
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 // Process form
+                
+                // Sanitize POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                // Init data
+                $data = [
+                    'name' => trim($_POST['name']),
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'confirm_password' => trim($_POST['confirm_password']),
+                    'name_error' => '',
+                    'email_error' => '',
+                    'password_error' => '',
+                    'confirm_password_err' => ''
+                ];
+
+                // Validate Name
+                if(empty($data['name'])){
+                    $data['name_err'] = 'Please enter name';
+                }
+
+                // Validate Email
+                if(empty($data['email'])){
+                    $data['email_err'] = 'Please enter email';
+                }
+
+                // Validate Password
+                if(empty($data['password'])){
+                    $data['password_err'] = 'Please enter password';
+                } elseif(strlen($data['password'])<6){
+                    $data['password_err'] = 'Password must be at least 6 caracters';
+                }
+
+                // Validate Confirm Password
+                if(empty($data['confirm_password'])){
+                    $data['confirm_password_err'] = 'Please confirm password';
+                } else{
+                    if($data['password'] != $data['confirm_password']){
+                        $data['confirm_password_err'] = 'Passwords do not match';
+                    }
+                }
+                
+                // Make sure errors are empty
+                if(empty($data['name_err']) && empty($data['email_err']) && empty($data['password_err']) && 
+                empty($data['confirm_password_err'])){
+                    // Validated
+                    die('SUCCESS');
+                } else {
+                    // Load view with errors
+                    $this->view('users/register', $data);
+                }
+
             } else {
                 // Init data
                 $data = [
