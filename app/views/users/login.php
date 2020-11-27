23615a2d832ebf2a0093e5b43fb5eb92572dbcf2
Youness <elkhatabi.youness@gmail.com>
2020-11-27 15:05:32 +0100

    correct action and class values

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -4,11 +4,11 @@
             <div class="card card-body bg-light mt-5">
                 <h2>Login</h2>
                 <p>Please fill in your credentials to log in</p>
-                <form action="<?php echo URLROOT ?>/users/register" method="post">
+                <form action="<?php echo URLROOT ?>/users/login" method="post">
                     <div class="form-group">
                         <label for="email">Email: <sup>*</sup></label>
-                        <input type="email" name="email" class="form-control form-control-lg" 
-                        <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>
+                        <input type="email" name="email" class="form-control form-control-lg
+                        <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"
                         value="<?php echo $data['email'];?>">
                         <span class="invalid-feedback">
                             <?php echo $data['email_err']; ?>
@@ -16,8 +16,8 @@
                     </div>
                     <div class="form-group">
                         <label for="password">Password: <sup>*</sup></label>
-                        <input type="password" name="password" class="form-control form-control-lg" 
-                        <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>
+                        <input type="password" name="password" class="form-control form-control-lg 
+                        <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"
                         value="<?php echo $data['password'];?>">
                         <span class="invalid-feedback">
                             <?php echo $data['password_err']; ?>
