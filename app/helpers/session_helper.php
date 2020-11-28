cf943a5972875c048c7005b5157241edde481ae8
Youness <elkhatabi.youness@gmail.com>
2020-11-28 19:18:26 +0100

    correct parameter values

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -1,5 +1,5 @@
 <?php
-    session_reset();
+    session_start();
 
     // Flash message helper
     // EXAMPLE - flash('register_success', 'You are now registered');
@@ -7,13 +7,13 @@
     function flash($name='', $message='', $class='alert alert-success'){
         if(!empty($name)){
             if(!empty($message) && empty($_SESSION[$name])){
-                if(!empty($_SESSION['name'])){
-                    unset($_SESSION['name']);
+                if(!empty($_SESSION[$name])){
+                    unset($_SESSION[$name]);
                 }
                 if(!empty($_SESSION[$name. '_class'])){
                     unset($_SESSION[$name. '_class']);
                 }
-                $_SESSION[$name] = $name;
+                $_SESSION[$name] = $message;
                 $_SESSION[$name. '_class'] = $class;
             } elseif(empty($message) && !empty($_SESSION[$name])){
                 $class = !empty($_SESSION[$name. '_class']) ? $_SESSION[$name. '_class'] : '';
@@ -23,7 +23,3 @@
             }
         } 
     }
-
-    $_SESSION['register_success']
-
-    
