1e211ad8730e03e7a88778e09bbf5dbb9afbab4b
Youness <elkhatabi.youness@gmail.com>
2020-11-28 14:08:45 +0100

    apply register function into controller

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -64,7 +64,17 @@ class Users extends Controller
                 empty($data['confirm_password_err'])
             ) {
                 // Validated
-                die('SUCCESS');
+                
+                // Hash Password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                // Register User
+                if($this->userModel->register($data)){
+                    redirect('users/login');
+                } else {
+                    die('Something went wrong !');
+                }
+
             } else {
                 // Load view with errors
                 $this->view('users/register', $data);
